3fcbbb29a55930b325fb87556f6462fb63af5da0
Luca Knaack <l.knaack93@gmail.com>
2024-07-29 19:39:16 +0200

    legal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { ProtectedUploadComponent } from './protected-upload-component/protected-upload.component';
 import { AdminComponent } from './admin/admin.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
+import { DataPrivacyComponent } from './data-privacy/data-privacy.component';
 
 export const routes: Routes = [
     {path: '', component: ProtectedUploadComponent},
-    {path: 'admin', component: AdminComponent}
+    {path: 'admin', component: AdminComponent},
+    {path: 'impressum', component: ImprintComponent},
+    {path: 'nutzungsbedingungen', component: TermsOfServiceComponent},
+    {path: 'datenschutz', component: DataPrivacyComponent}
 ];
